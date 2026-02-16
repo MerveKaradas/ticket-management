b80f57e20da869e96998269354cb32740eb80676
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2026-02-16 15:17:50 +0300

    User model created

---
+++ src/main/java/com/kafein/ticket_management/model/User.java
@@ -0,0 +1,104 @@
+package com.kafein.ticket_management.model;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.kafein.ticket_management.model.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Builder.Default
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // Security yetki kontrolü için kullanıcı rolü
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role));
+    }
+
+    // @Override
+    // public @Nullable String getPassword() {
+    // // TODO Auto-generated method stub
+    // throw new UnsupportedOperationException("Unimplemented method
+    // 'getPassword'");
+    // }
+
+    @Override
+    public String getUsername() {
+        // // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method
+        // 'getUsername'");
+        return this.email;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // Hesap süresi dolmamış
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true; // Hesap kilitli değil
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // Şifre süresi dolmamış
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true; // Kullanıcı aktif
+    }
+}
